4d2da15f7f851e514e0d7c1f1a4671df29c6e7ba
cgxxv <cgsixv@gmail.com>
2024-02-27 23:10:04 +0800

    Add readme, bind socket

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple Http Server
+
+This is a project for myself to learn c and c++ programing.
+
+## Man page tips
+
+* Section 1: user commands, e.g. `ls`, `cp`
+* Section 2: system calls, e.g. `open`, `read`
+* Section 3: library functions, e.g. `printf`, `malloc`
+* Section 4: special files, e.g. `/dev/null`, `/dev/sda`
+* Section 5: file formats, e.g. `/etc/passwd`, `fstab`
+* Section 6: games.
+* Section 7: conventions and miscellany, e.g. macro packages, protocol.
+* Section 8: administration and privileged commands, e.g. `mount`, `fsck`
+* Section L: math library functions.
+* Section N: tcl functions.
+
+> You can use the follow command in the terminal for document reference.
+```shell
+man socket
+man 2 getcwd
+```
+The number following with man means the section above.
+
+[linux man pages references](https://linux.die.net/man/)
--- src/server.h
+++ src/server.h
@@ -1,29 +1,19 @@
+#ifndef _SIMPLE_HTTP_SERVER_H
+#define _SIMPLE_HTTP_SERVER_H
+
 #include <arpa/inet.h>
-#include <ctype.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
-#define ISspace(x) isspace((int)(x))
-#define SERVER_STRING "Server: SongHao's http/0.1.0\r\n" // 定义个人server名称
+void *handle_request(void *sock);
+void die(const char *s);
+void get_addr(char *addr, struct sockaddr *address);
+void set_socket(int *server_sock, u_short *port);
 
-void *accept_request(void *client);
-void bad_request(int);
-void cat(int, FILE *);
-void cannot_execute(int);
-void error_die(const char *);
-void execute_cgi(int, const char *, const char *, const char *);
-int get_line(int, char *, int);
-void headers(int, const char *);
-void not_found(int);
-void serve_file(int, const char *);
-int startup(u_short *);
-void unimplemented(int);
+#endif
