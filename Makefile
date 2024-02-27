15ab5a4afb300528cd04056837eb1cfc4231f389
cgxxv <cgsixv@gmail.com>
2024-02-27 19:38:10 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+CC = gcc
+CFLAGS = -W -Wall -Wextra -Werror
+
+TARGET = simple-http-server
+
+SOURCES_DIR = src
+SOURCES = $(wildcard $(SOURCES_DIR)/*.c)
+
+OBJECTS_DIR = obj
+OBJECTS = $(patsubst $(SOURCES_DIR)/*.c, $(OBJECTS_DIR)/%.o, $(SOURCES))
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $^ -o $@
+
+$(OBJECTS_DIR)/%.o: $(SOURCES_DIR)/%.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rvf $(OBJECTS_DIR) $(TARGET)
+
+.PHONY: all clean
---
+++ src/server.h
@@ -0,0 +1,29 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ISspace(x) isspace((int)(x))
+#define SERVER_STRING "Server: SongHao's http/0.1.0\r\n" // 定义个人server名称
+
+void *accept_request(void *client);
+void bad_request(int);
+void cat(int, FILE *);
+void cannot_execute(int);
+void error_die(const char *);
+void execute_cgi(int, const char *, const char *, const char *);
+int get_line(int, char *, int);
+void headers(int, const char *);
+void not_found(int);
+void serve_file(int, const char *);
+int startup(u_short *);
+void unimplemented(int);
