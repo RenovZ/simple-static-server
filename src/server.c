d78a6a5fcc25f9d0baf1f8289024458ef8fbe6a8
cgxxv <cgsixv@gmail.com>
2024-02-29 01:30:12 +0800

    Add logmsg and write_response_header

--- src/server.c
+++ src/server.c
@@ -1,28 +1,81 @@
 #include "server.h"
-#include <stdio.h>
-#include <sys/_types/_ssize_t.h>
+#include <string.h>
 #include <sys/socket.h>
 
 void *handle_request(void *client_sock) {
   int sock = *(int *)client_sock;
-  char buf[1024];
-  int packets_len;
-  // char method[255];
-  // char url[1024];
-  // char path[512];
-  // size_t i, j;
+  char buf[BUFFER_SIZE];
+  size_t numchars;
+  char method[255];
+  char url[BUFFER_SIZE];
+  char path[BUFFER_SIZE];
+  size_t i, j;
 
-  packets_len = read_line(sock, buf, sizeof(buf));
-  printf("got %d chars from socket: %s\n", packets_len, buf);
+  numchars = read_request(sock, buf, sizeof(buf));
+  i = 0, j = 0;
+  while (!isspace(buf[i]) && i < sizeof(method) - 1) {
+    method[i] = buf[j];
+    i++;
+    j++;
+  }
+  method[i] = '\0';
+
+  i = 0;
+  while (isspace(buf[j]) && j < numchars) {
+    j++;
+  }
+  while (!isspace(buf[j]) && i < sizeof(url) - 1 && j < numchars) {
+    url[i] = buf[j];
+    i++;
+    j++;
+  }
+  url[i] = '\0';
+  logmsg("[%s] %s", method, url);
+
+  path[0] = '.';
+  path[1] = '\0';
+  sprintf(path + strlen(path), "%s", url);
+  path[strlen(url) + 2] = '\0';
+  serve_file(sock, path);
+
+  close(sock);
 
   return NULL;
 }
 
+void serve_file(int client_sock, const char *dir) {
+  char buf[BUFFER_SIZE];
+  ssize_t bytes_read;
+  int fd;
+
+  printf("directory: [%s]\n", dir);
+  if ((fd = open(dir, O_RDONLY)) == -1) {
+    die("failed opening dir");
+  }
+
+  write_response_header(client_sock);
+
+  while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0) {
+    send(client_sock, buf, bytes_read, 0);
+  }
+}
+
+void write_response_header(int client_sock) {
+  char buf[BUFFER_SIZE];
+
+  strcpy(buf, "HTTP/1.0 200 OK\r\n");
+  send(client_sock, buf, strlen(buf), 0);
+  strcpy(buf, "Content-Type: text/html\r\n");
+  send(client_sock, buf, strlen(buf), 0);
+  strcpy(buf, "\r\n");
+  send(client_sock, buf, strlen(buf), 0);
+}
+
 // parse a row of http packets
-int read_line(int sock, char *buf, int size) {
+int read_request(int sock, char *buf, int size) {
   int i = 0;
   char c = '\0';
-  int n;
+  ssize_t n;
 
   while (i < size - 1 && c != '\n') {
     n = recv(sock, &c, 1, 0);
@@ -100,23 +153,43 @@ void startup(int *server_sock, u_short *port) {
   return;
 }
 
+void logmsg(const char *format, ...) {
+  va_list args;
+  struct timeval tv;
+  struct tm *time_info;
+  char time_str[80];
+
+  gettimeofday(&tv, NULL);
+  time_info = localtime(&tv.tv_sec);
+  strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", time_info);
+  sprintf(time_str + strlen(time_str), ".%06d", tv.tv_usec);
+
+  printf("%s\t", time_str);
+
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+
+  printf("\n");
+}
+
 int main(void) {
   int server_sock = -1;
   u_short port = 8000;
   int client_sock = -1;
   struct sockaddr client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
-  char s_addr[1024];
+  char s_addr[BUFFER_SIZE];
   pthread_t new_thread;
 
   startup(&server_sock, &port);
 
-  printf("server started on port %d, with socket %d\n", port, server_sock);
+  logmsg("      started on port %d, with socket %d", port, server_sock);
   while (1) {
     client_sock = accept(server_sock, &client_addr, &client_addr_len);
 
     get_addr(s_addr, &client_addr);
-    printf("%s connected\n", s_addr);
+    logmsg("      connected by %s", s_addr);
 
     if (client_sock == -1)
       die("failed accepting request");
--- src/server.h
+++ src/server.h
@@ -2,19 +2,30 @@
 #define _SIMPLE_HTTP_SERVER_H
 
 #include <arpa/inet.h> // used for converting values between host and network byte order
+#include <ctype.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 void *handle_request(void *client_sock);
-int read_line(int sock, char *buf, int size);
+void serve_file(int client_sock, const char *dir);
+void write_response_header(int client_sock);
+int read_request(int sock, char *buf, int size);
 void die(const char *s);
 void get_addr(char *addr, struct sockaddr *address);
 void startup(int *server_sock, u_short *port);
+void logmsg(const char *format, ...);
 
 #endif
